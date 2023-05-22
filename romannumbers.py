495fa394dcf40f0b88457405d7119ae3c391d8af
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-05-22 22:05:22 +0100

    Romanos hasta 3999

--- romannumbers.py
+++ romannumbers.py
@@ -1,18 +1,82 @@
-simbolos = {
+unidades = {
     1 : "I",
-    5 : "V"
+    5 : "V",
+    10: "X"
+}
+decenas = {
+    1 : "X",
+    5 : "L",
+    10: "C"
 }
 
-def entero_a_romano(n_int):
-    if n_int in simbolos:
-        return simbolos[n_int]
-    if n_int < 4:
-        return n_int * simbolos[1]
-    elif n_int == 4:
-        return simbolos[1] + simbolos[5]
+centenas = {
+    1: "C",
+    5: "D",
+    10: "M"
+}
+
+miles = {
+    1: "M"
+}
+
+class RomanNumberError(Exception):
+    pass
+
+def listar_numero(num):
+    n_mil = num // 1000 * 1000
+    n_cent = (num % 1000) // 100 * 100
+    n_dec = (num % 100) // 10 * 10
+    n_uni = (num % 10)
+
+    return n_mil, n_cent, n_dec, n_uni
+
+def sacar_clave(num):
+    if num >=1000:
+        clave = miles
+        num = num // 1000
+    
+    elif num >= 100:
+        clave = centenas
+        num = num // 100
+
+    elif num >= 10:
+        clave = decenas
+        num = num // 10
+    else: 
+        clave = unidades
+    return clave, num
+
+def sacar_simbolo_digito(clau, digit):
+    simbol_digit = ""
+    if digit < 4:
+        simbol_digit += digit * clau[1]
+    elif digit == 4:
+        simbol_digit += clau[1] + clau[5]
+    elif digit < 9:
+        num_unidades = digit - 5
+        simbol_digit += clau[5] + num_unidades * clau[1]
     else:
-        multiplicador = n_int - 5
-        return simbolos[5] + multiplicador * simbolos[1]
+        simbol_digit += clau[1] + clau[10]
+    return simbol_digit 
+
+
+def entero_a_romano(n_int):
+    if n_int > 3999:
+        raise RomanNumberError("RomanNumber must be less of 4000")
+    
+    digitos = listar_numero(n_int)
+
+    resultado = ''
+
+    for digito in digitos:
+        if digito == 0:
+            continue # te ignora el siguiente codigo del for y te coge el siguiente digito
+        
+        clave, digito = sacar_clave(digito)
+
+        resultado += sacar_simbolo_digito(clave, digito)
+
+    return resultado
 
 if __name__ == "__main__": #Esto es para que solo ejecute el codigo que sigue cuando ejecutemos el programa desde romannumbers, pero en otros ficheros donde se importe no se va a ejecutar
-    print(entero_a_romano(6))
+    print(entero_a_romano(0))
--- test_romannumbers.py
+++ test_romannumbers.py
@@ -1,4 +1,5 @@
-from romannumbers import entero_a_romano
+from romannumbers import entero_a_romano, RomanNumberError
+import pytest
 
 def test_uno_es_palito():
     assert entero_a_romano(1) == "I"
@@ -23,3 +24,35 @@ def test_siete_es_uve_palito_palito():
 
 def test_ocho_es_uve_palito_tres_veces():
     assert entero_a_romano(8) == "VIII"
+
+def test_nueve_es_palito_x():
+    assert entero_a_romano(9) == "IX"
+
+def test_diez_es_x():
+    assert entero_a_romano(10) == "X"
+
+def test_veinte_es_2x():
+    assert entero_a_romano(20) == "XX"
+
+def test_resto_decenas():
+    assert entero_a_romano(30) == "XXX"
+    assert entero_a_romano(40) == "XL"
+    assert entero_a_romano(50) == "L"
+    assert entero_a_romano(60) == "LX"
+    assert entero_a_romano(70) == "LXX"
+    assert entero_a_romano(80) == "LXXX"
+    assert entero_a_romano(90) == "XC"
+
+def test_once_x_palito():
+    assert entero_a_romano(11) == "XI"
+
+def test_101_es_Cpalito():
+    assert entero_a_romano(101) == "CI"
+
+def test_2345_es_MMCCCXLV():
+    assert entero_a_romano(2345) == "MMCCCXLV"
+    assert entero_a_romano(3999) == "MMMCMXCIX"
+
+def test_4000_es_error():
+    with pytest.raises(RomanNumberError):
+        entero_a_romano(4000)
