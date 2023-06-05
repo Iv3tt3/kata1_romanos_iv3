1a24b3f1bbf5d2968ef7158f6624e9246ad7f05e
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-06-05 19:24:08 +0100

    Clase romanos con metodos logica y aritmeticos

--- class_romannumbers.py
+++ class_romannumbers.py
@@ -35,7 +35,7 @@ from romannumbers import *
 
 class RomanNumber:
     def __init__(self, entrada):
-        if type(entrada) == int:
+        if type(entrada) == int and entrada >0:
             #cuando pones un _ delante estas diciendo que no los usen
             self._numero = entrada
             self._simbolo = entero_a_romano(entrada)
@@ -43,7 +43,7 @@ class RomanNumber:
             self._numero = Romano_a_Entero(entrada)
             self._simbolo = entrada
         else:
-            raise RomanNumberError("Se debe introducir un entero o romano valido")
+            raise RomanNumberError("Se debe introducir un entero mayor que cero o un romano valido")
     
     @property
     def numero(self):
@@ -63,12 +63,77 @@ class RomanNumber:
         self._numero = Romano_a_Entero(entrada)
         self._simbolo = entrada
 
-    #Metodos magicos para aritmetica
+
+    def __to_roman(self, otro):
+        #Este metodo queremos que sea privado por lo que lo indicamos con __
+        if type(otro) != RomanNumber:
+            otro = RomanNumber(otro)
+        return otro
+
+    #Metodos magicos para la logica
+
 
     def __eq__(self, other):
         #Este metodo es necesario para comparar instancias, es decir, el resultado de numero * otro numero saca una instancia y se compara con otra instancia guardada en otro espacio de memoria. Aunque sea iguales, python no los ve iguales. Por eso es necesario este metodo.
+        other = self.__to_roman(other)
         return self.numero == other.numero
+    
+    #def __req__(self, other):
+        #reverse equal
+       # other = self.__to_roman(other)
+       # return self.__eq__(other)
+    
+    def __lt__(self, other):
+        #less
+        other = self.__to_roman(other)
+        return self.numero < other.numero
+    
+    def __le__(self, other):
+        #less and equal
+        other = self.__to_roman(other)
+        return self.numero <= other.numero
+    
+    def __gt__(self, other):
+        #grater
+        other = self.__to_roman(other)
+        return self.numero > other.numero
+    
+    def __ge__(self, other):
+        #grater and equal
+        other = self.__to_roman(other)
+        return self.numero >= other.numero
 
+    def __ne__(self, other):
+        #not equal
+        other = self.__to_roman(other)
+        return self.numero != other.numero
+    
+    
+    #Metodos magicos para aritmetica
+
+    #Sumar
+    def __add__(self, otro):
+        otro = self.__to_roman(otro)
+        resultado = self.numero + otro.numero
+        return RomanNumber(resultado)
+
+    def __radd__(self, otro):
+        return self.__add__(otro)
+
+    #Restar
+
+    def __sub__(self, otro):
+        otro = self.__to_roman(otro)
+        if self.numero <= otro.numero:
+            raise RomanNumberError("Resta no permitida, el resultado no existe en numeros romanos porque da un valor negativo o cero")
+        resultado = self.numero - otro.numero
+        return RomanNumber(resultado)
+
+    def __rsub__(self, otro):
+        otro = self.__to_roman(otro)
+        return otro.__sub__(self)
+
+    #Multiplicar
     def __mul__(self, otro):
         if not isinstance(otro, RomanNumber):
             otro = RomanNumber(otro)
@@ -76,10 +141,36 @@ class RomanNumber:
         return RomanNumber(resultado)
     
     def __rmul__(self, otro):
+        #En self mete el romano1 y en otro el 5 aunque en el test linea 54 hemos puesto assert 5 * romano1 porque el rmult es el mult reverse
         return self.__mul__(otro)
-    
+
+
+    #Dividir
+    def __truediv__(self, otro):
+        otro = self.__to_roman(otro)
+        resultado = int(self.numero / otro.numero)
+        #Nunca dividira entre cero por el AND añadido en linia 38
+        if self.numero % otro.numero != 0:
+            raise RomanNumberError("Division no permitida. La division no es exacta o entera, el resultado es un numero decimal")
+            #resultado = int(resultado)
+            #decimales = (self.numero / otro.numero) - resultado
+            #print("La division no es entera o exacta, los decimales son: ", decimales)
+        return RomanNumber(resultado)
+
+    def __rtruediv__(self, otro):
+        otro = self.__to_roman(otro)
+        return otro.__truediv__(self)
+
+    #Metodos magicos representacion
+
     def __repr__(self):
         return f"{self.numero} [{self.simbolo}]"
     
     def __str__(self):
         return self.__repr__()
+
+
+
+if __name__ == "__main__":
+    pass
+
--- test_class_romannumber.py
+++ test_class_romannumber.py
@@ -1,4 +1,5 @@
-from class_romannumbers import RomanNumber
+from class_romannumbers import RomanNumber, RomanNumberError
+import pytest
 
 def test_instanciar_un_numero_romano():
     romano = RomanNumber(23)
@@ -32,6 +33,31 @@ def test_cambiar_valor_de_romano():
     assert romano.numero == 3
     assert romano.simbolo == "III"
 
+
+def test_sumas():
+    romano1 = RomanNumber("X")
+    romano2 = RomanNumber(5)
+
+    assert romano1 + romano2 == RomanNumber('XV')
+    assert romano1 + 5 == RomanNumber(15)
+    assert romano1 + "V" == RomanNumber(15)
+
+    assert 5 + romano1 == RomanNumber('XV')
+
+def test_restas():
+    romano1 = RomanNumber("X")
+    romano2 = RomanNumber(5)
+
+    assert romano1 - romano2 == RomanNumber('V')
+    assert romano1 - 5 == RomanNumber(5)
+    assert romano1 - "V" == RomanNumber(5)
+
+    assert 10 - romano2 == RomanNumber('V')
+    
+    with pytest.raises(RomanNumberError):
+        assert 5 - romano1
+
+
 def test_multiplicacionens():
     romano1 = RomanNumber("X")
     romano2 = RomanNumber(5)
@@ -43,3 +69,47 @@ def test_multiplicacionens():
 
     assert 5 * romano1 == RomanNumber(50)
 
+def test_divisiones():
+    romano1 = RomanNumber("X")
+    romano2 = RomanNumber(5)
+
+    assert romano1 / romano2 == RomanNumber('II')
+    assert romano1 / 5 == RomanNumber(2)
+    assert romano1 / "V" == RomanNumber(2)
+
+    assert 10 / romano2 == RomanNumber('II')
+    
+    with pytest.raises(RomanNumberError):
+        assert 5 / romano1
+
+def test_zero():
+    with pytest.raises(RomanNumberError):
+        assert RomanNumber(0)
+
+def test_operaciones_logicas():
+    romano1 = RomanNumber(10)
+
+    assert romano1 == 10
+
+    assert romano1 > 9
+    assert romano1 >= 9
+    assert romano1 >= 10
+
+    assert romano1 < 11
+    assert romano1 <= 11
+    assert romano1 <= 10
+
+    assert romano1 != 11
+
+
+    assert 10 == romano1
+
+    assert 11 > romano1
+    assert 11 >= romano1
+    assert 10 >= romano1
+
+    assert 9 < romano1
+    assert 9 <= romano1
+    assert 10 <= romano1
+
+    assert 11 != romano1
